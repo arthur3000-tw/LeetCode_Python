c0245db18a7195c5daf51dabad6f3733b46dd87f
arthur3000-tw <49877804+arthur3000-tw@users.noreply.github.com>
2024-09-30 15:53:13 +0800

    Create 128. Longest Consecutive Sequence v1.py

---
+++ 128. Longest Consecutive Sequence v1.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_dict = {}
+        length = 0
+
+        for num in nums:
+            nums_dict[num] = False
+
+        for num in nums:
+            count = 1
+            if nums_dict[num]:
+                continue
+            nums_dict[num] = True
+
+            next_num = num + 1
+            while next_num in nums_dict:
+                nums_dict[num+1] = True
+                count += 1
+                next_num += 1
+            
+            next_num = num - 1
+            while next_num in nums_dict:
+                nums_dict[num-1] = True
+                count += 1
+                next_num -= 1
+            
+            if count > length:
+                length = count
+        
+        return length
