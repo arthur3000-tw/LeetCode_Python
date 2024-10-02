795b3e2a5fe0f5914c7ba8697f58251679f4f37b
arthur3000-tw <49877804+arthur3000-tw@users.noreply.github.com>
2024-10-03 01:46:37 +0800

    Create 875. Koko Eating Bananas v1.py

---
+++ 875. Koko Eating Bananas v1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        while True:
+            mid = (left + right) // 2
+            hour = 0
+            for pile in piles:
+                hour += math.ceil(pile / mid)
+            if hour <= h:
+                if left == mid:
+                    return mid
+                right = mid
+            else:
+                left = mid + 1
