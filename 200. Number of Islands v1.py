89cde2050f6ad58b289114b46ef46f7bf32b3b54
arthur3000-tw <49877804+arthur3000-tw@users.noreply.github.com>
2024-10-19 11:13:55 +0800

    Update 200. Number of Islands v1.py

--- 200. Number of Islands v1.py
+++ 200. Number of Islands v1.py
@@ -1,6 +1,7 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # 處理邊界問題，確認座標有效性
         def valid_point(point,grid):
             i = point[0]
             j = point[1]
@@ -14,27 +15,37 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
+        # 走遍所有座標
         for i in range(m):
             for j in range(n):
+                # 有無 island 的 flag
                 flag = False
+                # 確認座標有無看過
                 if (i,j) not in seen:
                     check_queue.append((i,j))
                     seen.add((i,j))
+                # 遍尋 queue
                 while check_queue:
                     point = check_queue.pop()
                     value = grid[point[0]][point[1]]
+                    # 確認座標位置是否為 island
                     if value == "1":
+                        # 將 flag 標記為 true
                         flag = True
-                        # arounds
+                        # 計算鄰近座標（上、下、左、右）
                         top = (point[0]-1,point[1])
                         down = (point[0]+1,point[1])
                         left = (point[0],point[1]-1)
                         right = (point[0],point[1]+1)
+                        # 鄰近座標放入 list 中
                         arounds = [top, down, left, right]
+                        # 遍尋每個鄰近座標
                         for position in arounds:
+                            # 確認座標有效性及是否看過此座標
                             if valid_point(position,grid) and position not in seen:
                                 check_queue.append(position)
                                 seen.add(position)
+                # 遍尋 queue 後，確認 flag 是否為 true
                 if flag == True:
                     islands += 1
 
